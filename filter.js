1ee018a7024c381d09b3fc21bd80cfa592582b25
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-01 01:55:49 +0600

    Update Array Filter odd number condition

--- filter.js
+++ filter.js
@@ -4,7 +4,7 @@ const players = [75, 65, 67, 72, 55, 59];
 // const selected = players.filter(p => p > 70);
 // const selected = players.filter(p => p > 80);
 // const selected = players.filter(p => p > 50);
-const selected = players.filter( p => p % 2 == 1);
+const selected = players.filter( p => p % 2 === 1);
 console.log(selected);
 
 const friends = ['Tom', 'John', 'Micheal', 'Oliver', 'Tim', 'Joshna'];
