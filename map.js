290eef9482023359037e42dba50262af41e233c7
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-01 01:14:17 +0600

    Array Map

--- map.js
+++ map.js
@@ -1,10 +1,28 @@
-const numbers = [ 4, 5, 2, 8, 10];
+// map ==> loops through each element of the array and do operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array
 
+const numbers = [4, 5, 2, 8, 10];
 
-const doubled = [];
-for(const num of numbers){
-    const double = num * 2;
-    doubled.push(double)
+function doubleIt(num) {
+    // console.log('num now', num);
+    return num * 2;
 }
 
-console.log(doubled);
+const double2 = n => n * 2;
+
+const result = numbers.map(doubleIt)
+// console.log(result);
+
+const output = numbers.map(double2);
+// console.log(output);
+
+const output2 = numbers.map(n => n * 2);
+console.log(output2);
+
+
+// const doubled = [];
+// for(const num of numbers){
+//     const double = num * 2;
+//     doubled.push(double)
+// }
+
+// console.log(doubled);
