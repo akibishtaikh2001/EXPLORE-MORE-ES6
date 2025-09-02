9ef33b2365b6431b1b6b02708af4d8024d3f4d95
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-02 21:30:16 +0600

    Complate practice task 2 use find  and filter and what is the diffrent between

---
+++ practice2.js
@@ -0,0 +1,9 @@
+const numbers = [33, 50, 79, 78, 90, 101, 30];
+
+const divisibleBY10 = numbers.filter(divide => divide % 10 === 0)
+   
+
+console.log(divisibleBY10);
+
+const find = numbers.find(divided => divided % 10 === 0);
+console.log(find);
