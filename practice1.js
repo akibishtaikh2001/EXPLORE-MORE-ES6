9553a665d6ea15721305a8d855161603d15506f4
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-09-02 21:09:15 +0600

    Complate practice 1  map and for loop Array Numbers

---
+++ practice1.js
@@ -0,0 +1,17 @@
+const numbers = [1, 3, 5, 7, 9];
+const odd = [];
+for(const number of numbers){
+    let num = 0;
+  if(number % 2 !== 0){
+    num = number + 1; 
+  }
+    odd.push(num)
+}
+
+console.log(odd);
+
+
+//  Map
+
+const odd2 = numbers.map(num => num + 1);
+console.log(odd2);
